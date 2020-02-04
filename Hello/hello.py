645cb6c97714bd5af0cacafca608c8ec4ea3f70d
obijuan <juan@iearobotics.com>
2020-02-04 14:35:02 +0100

    My first python program

--- Hello/hello.py
+++ Hello/hello.py
@@ -1,5 +1,5 @@
 print("This is my first python program in pycharm")
-print("Hello world!!!! Changed!!!!!!!")
+print("Hello world!!!! Changed!!!!!!!--")
 
 # -- Exercise S1-1
 print("Another print added!")
