9a6e0b1e7a577914a1b619324abfca4afc5b3580
Obijuan <juan@iearobotics.com>
2020-02-12 08:52:56 +0100

    P0. Exercise 1

---
+++ P0/Ex1.py
@@ -0,0 +1,7 @@
+from Seq0 import *
+
+# -- Call the seq_ping function that is
+# -- located in the Seq0 module
+# -- You should see the "OK" string on the console
+print("Testing the seq_ping() funcion")
+seq_ping()
