cebd5c52d39264c6fe6694763cfbb832171eec48
Obijuan <juan@iearobotics.com>
2020-02-19 09:49:27 +0100

    P1: Ex5

---
+++ P1/Ex6.py
@@ -0,0 +1,19 @@
+from Seq1 import Seq
+
+PRACTICE = 1
+EXERCISE = 6
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Create a Null sequence
+s1 = Seq()
+
+# -- Create a valid sequence
+s2 = Seq("ACTGA")
+
+# -- Create an invalid sequence
+s3 = Seq("Invalid sequence")
+
+for i, s in enumerate([s1, s2, s3]):
+    print(f"Sequence {i}: (Length: {s.len()}) {s}")
+    print(f"  Bases: {s.count()}")
