da29a191245f7ad29b610cacab5793503af6f876
Obijuan <juan@iearobotics.com>
2020-02-19 09:03:33 +0100

    P1: Ex1

---
+++ P1/Ex2.py
@@ -0,0 +1,16 @@
+from Seq1 import Seq
+
+PRACTICE = 1
+EXERCISE = 2
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Create a Null sequence
+s1 = Seq()
+
+# -- Create a valid sequence
+s2 = Seq("ACTGA")
+
+print(f"Sequence 1: {s1}")
+print(f"Sequence 2: {s2}")
+
