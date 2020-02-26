8097b94a11ddefe53900cf968d8c03a7561f1e45
Obijuan <juan@iearobotics.com>
2020-02-26 09:29:44 +0100

    Session 08: Done!

---
+++ P2/EX2.py
@@ -0,0 +1,16 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 2
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "192.168.1.45"
+PORT = 8080
+
+# -- Create a client object
+c = Client(IP, PORT)
+
+# -- Print the IP and PORTs
+print(c)
