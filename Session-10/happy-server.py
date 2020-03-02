cc0f1941af4493c8c2d978c16f565fd6c13ecbc2
Obijuan <juan@iearobotics.com>
2020-03-02 14:43:20 +0100

    Session 10: Happy server added

---
+++ Session-10/happy-server.py
@@ -0,0 +1,67 @@
+import socket
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "192.168.124.179"
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+
+while True:
+    # -- Waits for a client to connect
+    print("Waiting for Clients to connect")
+
+    try:
+        (cs, client_ip_port) = ls.accept()
+
+    # -- Server stopped manually
+    except KeyboardInterrupt:
+        print("Server stopped by the user")
+
+        # -- Close the listenning socket
+        ls.close()
+
+        # -- Exit!
+        exit()
+
+    # -- Execute this part if there are no errors
+    else:
+
+        print("A client has connected to the server!")
+
+        # -- Read the message from the client
+        # -- The received message is in raw bytes
+        msg_raw = cs.recv(2048)
+
+        # -- We decode it for converting it
+        # -- into a human-redeable string
+        msg = msg_raw.decode()
+
+        # -- Print the received message
+        print(f"Message received: {msg}")
+
+        # -- Send a response message to the client
+        response = "HELLO. I am the Happy Server :-)\n"
+
+        # -- The message has to be encoded into bytes
+        cs.send(response.encode())
+
+        # -- Close the data socket
+        cs.close()
