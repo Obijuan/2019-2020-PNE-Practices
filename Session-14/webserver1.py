16e473c4c98c6663ff912b122b5883758afc40b3
Obijuan <juan@iearobotics.com>
2020-03-15 22:34:10 +0100

    Session 14: webserver1.py added

---
+++ Session-14/webserver1.py
@@ -0,0 +1,24 @@
+import http.server
+import socketserver
+
+# Define the Server's port
+PORT = 8080
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+# -- Use the http.server Handler
+Handler = http.server.SimpleHTTPRequestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Server Stopped!")
+        exit()
