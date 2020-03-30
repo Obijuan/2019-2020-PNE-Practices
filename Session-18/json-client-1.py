4ae7ec905c3c62076f85e3f38a9447c21bc7237c
Obijuan <juan@iearobotics.com>
2020-03-30 18:44:13 +0200

    Session 18: json-client-1 added

---
+++ Session-18/json-client-1.py
@@ -0,0 +1,31 @@
+# -- Example of a client that uses the HTTP.client library
+# -- for requesting the main page from the server
+import http.client
+
+PORT = 8080
+SERVER = 'localhost'
+
+print(f"\nConnecting to server: {SERVER}:{PORT}\n")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER, PORT)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", "/")
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+
+# -- Print the received data
+print(f"CONTENT: {data1}")
