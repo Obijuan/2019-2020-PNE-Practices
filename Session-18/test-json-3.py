822d53908e2e7bab0181d4596f46c930c3810637
Obijuan <juan@iearobotics.com>
2020-03-30 17:10:44 +0200

    Session 18: Example 3 added

---
+++ Session-18/test-json-3.py
@@ -0,0 +1,36 @@
+import json
+import termcolor
+from pathlib import Path
+
+# -- Read the json file
+jsonstring = Path("people-3.json").read_text()
+
+# Create the object person from the json string
+person = json.loads(jsonstring)
+
+# Person is now a dictionary. We can read the values
+# associated to the fields 'Firstname', 'Lastname' and 'age'
+
+# Print the information on the console, in colors
+print()
+termcolor.cprint("Name: ", 'green', end="")
+print(person['Firstname'], person['Lastname'])
+termcolor.cprint("Age: ", 'green', end="")
+print(person['age'])
+
+# Get the phoneNumber list
+phoneNumbers = person['phoneNumber']
+
+# Print the number of elements in the list
+termcolor.cprint("Phone numbers: ", 'green', end='')
+print(len(phoneNumbers))
+
+# Print all the numbers
+for i, num in enumerate(phoneNumbers):
+    termcolor.cprint("  Phone {}:".format(i), 'blue')
+
+    # The element num contains 2 fields: number and type
+    termcolor.cprint("    Type: ", 'red', end='')
+    print(num['type'])
+    termcolor.cprint("    Number: ", 'red', end='')
+    print(num['number'])
