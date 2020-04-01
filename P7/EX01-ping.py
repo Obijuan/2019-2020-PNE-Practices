f9b3810c9b263ffaaa5a670507c4a4f16493d221
Obijuan <juan@iearobotics.com>
2020-04-01 11:30:18 +0200

    Session 19: Exercises 1 and 2 added

---
+++ P7/EX01-ping.py
@@ -0,0 +1,40 @@
+import http.client
+import json
+
+SERVER = 'rest.ensembl.org'
+ENDPOINT = '/info/ping'
+PARAMS = '?content-type=application/json'
+URL = SERVER + ENDPOINT + PARAMS
+
+print()
+print(f"Server: {SERVER}")
+print(f"URL: {URL}")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER)
+
+try:
+    conn.request("GET", ENDPOINT + PARAMS)
+
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode()
+
+# -- Create a variable with the data,
+# -- form the JSON received
+response = json.loads(data1)
+
+ping = response['ping']
+
+if ping == 1:
+    print("PING OK! The database is running!")
+
---
+++ P7/EX02-gene-id.py
@@ -0,0 +1,22 @@
+import termcolor
+
+GENES = {
+    'FRAT1': 'ENSG00000165879',
+    'ADA': 'ENSG00000196839',
+    'FXN': 'ENSG00000165060',
+    'RNU6_269P': 'ENSG00000212379',
+    'MIR633': 'ENSG00000207552',
+    'TTTY4C': 'ENSG00000228296',
+    'RBMY2YP': 'ENSG00000227633',
+    'FGFR3': 'ENSG00000068078',
+    'KDR': 'ENSG00000128052',
+    'ANK2': 'ENSG00000145362',
+}
+
+print()
+print("Dictionary of Genes!")
+print(f"There are {len(GENES)} genes in the dicctionary:\n")
+
+for gene in GENES:
+    termcolor.cprint(f"{gene}", "green", end="")
+    print(f": --> {GENES[gene]}")
